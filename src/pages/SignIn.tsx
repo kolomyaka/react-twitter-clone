974351acec22be728a92937aaf2f13f198010350
kolomyaka1 <66018338+kolomyaka1@users.noreply.github.com>
2022-04-16 20:54:14 +0300

    Попытка перейти к использованию styled-components

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,28 +2,36 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { Input, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import Button from '@mui/material/Button'
 
 const Wrapper = styled('div')(
     ({ theme }) => `
-        display: flex;
-        height: 100vh;
+        padding: 0;
     `
 );
 
 const BlueSide = styled('div')(
     ({ theme }) => `
         background-color: #1DA1F2;
-        min-width: 50%;
-        padding: 25px;
+        flex:0 0 50%;
     `
 );
 
 const LoginSide = styled('div')(
     ({ theme }) => `
-        padding: 25px;
-        min-width: 50%;
+        flex:0 0 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    `
+)
+
+const ContentBox = styled('div')(
+    ({ theme }) => `
+        display: flex;
+        height: 100vh;
+
     `
 )
 
@@ -51,13 +59,24 @@ const LoginSideText = styled('span')(
     `
 )
 
+const LoginContent = styled('div')(
+    ({ theme }) => `
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    max-width: 380px;
+    `
+)
+
+
+
 
 
 function SignIn() {
 
   return (
     <Wrapper>
-
+        <ContentBox>
         <BlueSide>
             <BlueSideList>
                 <BlueSideListItem>Читайте о том, что вам интересно.</BlueSideListItem>
@@ -67,12 +86,15 @@ function SignIn() {
         </BlueSide>
 
         <LoginSide>
-            <TwitterIcon />
-            <Typography>Узнайте, что происходит в мире прямо сейчас</Typography>
-            <Typography>Присоединяйтесь к Твиттеру прямо сейчас!</Typography>
-            <Button variant='contained' color='primary' fullWidth>Зарегистрироваться</Button>
-            <Button variant='outlined' color='primary' fullWidth>Войти</Button>
+            <LoginContent>
+                <TwitterIcon fontSize='large' color='primary'/>
+                <Typography variant='h4'>Узнайте, что происходит в мире прямо сейчас</Typography>
+                <Typography>Присоединяйтесь к Твиттеру прямо сейчас!</Typography>
+                <Button variant='contained' color='primary' fullWidth>Зарегистрироваться</Button>
+                <Button variant='outlined' color='primary' fullWidth>Войти</Button>
+            </LoginContent>
         </LoginSide>
+        </ContentBox>
     </Wrapper>
   )
 }
