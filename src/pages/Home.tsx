03f60aeb0d3d3f1053961662e2c0085f4d70359e
kolomyaka1 <ka4ok24@mail.ru>
2022-04-19 00:33:04 +0300

    Создание компоненты поста

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,11 @@ import MessageIcon from '@mui/icons-material/MailOutline';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import ListIcon from '@mui/icons-material/ListAlt';
 import UserIcon from '@mui/icons-material/PersonOutline';
+import CommentIcon from '@mui/icons-material/ModeCommentOutlined';
+import RepeatIcon from '@mui/icons-material/RepeatOutlined';
+import LikeIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShareIcon from '@mui/icons-material/ReplyOutlined';
+
 
 const IconsList = styled("ul")`
     list-style: none;
@@ -44,7 +49,32 @@ const SearchTextField = styled(TextField)(({ theme }) => ({
 
 }))
 
-const userLogin = styled.div`
+const СontentTweetWrapper = styled.div`
+    margin: 7px 15px;
+    cursor: pointer;
+    &:hover {
+        background-color: rgb(245,248,250);
+    }
+`
+
+const FlexWrapper = styled('div')`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 450px;
+    
+`
+
+const TweetsCounter = styled('span')`
+    font-size: 20px;
+    padding-left: 5px;
+`
+
+const Tweet = styled('div')`
+    background-color: #000;
+    &:hover {
+        background-color: rgb(245, 248, 250);
+    }
 `
 
 
@@ -102,23 +132,45 @@ export const Home = () => {
                     </IconsList>
                 </Grid>
                 <Grid item xs={6}>
-                    <Paper sx={{ borderBottom: 0, borderTop: 0, height: '100vh' }} variant='outlined'>
-                        <Paper variant='outlined' sx={{ borderLeft: 0, borderRight: 0, padding: '10px 15px' }}>
-                            <Typography variant='h6' sx={{ fontWeight: 700, borderRadius: 0 }}>Главная</Typography>
-                        </Paper>
-                        <Paper variant='outlined'>
-                            <Grid container spacing={4}>
-                                <Grid item xs={1}>
-                                    <img src='https://i.pravatar.cc/45' style={{ borderRadius: '50%', margin: '7px 10px' }} alt="" />
-                                </Grid>
-                                <Grid item xs={11}>
-                                    <Typography sx={{ margin: '7px 15px' }}><b>Kolomyaka</b><span>@Kolomyaka</span></Typography>
-                                </Grid>
-                            </Grid>
-                        </Paper>
-                    </Paper>
+                    <Tweet>
+                        <Paper sx={{ borderBottom: 0, borderTop: 0, height: '100vh' }} variant='outlined'>
+                            <Paper variant='outlined' sx={{ borderLeft: 0, borderRight: 0, padding: '10px 15px' }}>
+                                <Typography variant='h6' sx={{ fontWeight: 700, borderRadius: 0 }}>Главная</Typography>
+                            </Paper>
+                            <Paper variant='outlined'>
+                                <Grid container spacing={4}>
+                                    <Grid item xs={1}>
+                                        <img src='https://i.pravatar.cc/45' style={{ borderRadius: '50%', margin: '7px 10px' }} alt="" />
+                                    </Grid>
+                                    <Grid item xs={11}>
+                                        <СontentTweetWrapper>
+                                            <Typography><b>Kolomyaka</b><span style={{ color: '#9e9e9e', marginLeft: '5px' }}>@Kolomyaka</span></Typography>
+                                            <Typography variant='body1'>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ducimus voluptatibus obcaecati eos repellendus ex quod fugit molestiae consectetur nesciunt magni nemo dolores architecto eaque id voluptatum, minus quidem nulla.
+                                            </Typography>
+                                            <FlexWrapper>
+                                                <IconButton>
+                                                    <CommentIcon style={{ fontSize: '16px' }} />
+                                                    <TweetsCounter>1</TweetsCounter>
+                                                </IconButton>
+                                                <IconButton>
+                                                    <RepeatIcon style={{ fontSize: '16px' }} />
+                                                </IconButton>
+                                                <IconButton>
+                                                    <LikeIcon style={{ fontSize: '16px' }} />
+                                                </IconButton>
+                                                <IconButton>
+                                                    <ShareIcon style={{ fontSize: '16px' }} />
+                                                </IconButton>
+                                            </FlexWrapper>
+                                        </СontentTweetWrapper>
 
+                                    </Grid>
+                                </Grid>
+                            </Paper>
+                        </Paper>
 
+                    </Tweet>
                 </Grid>
                 <Grid item xs={3}>
                     <SearchTextField
