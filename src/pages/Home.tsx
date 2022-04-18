581f3255f133db699accec9fe5032c783cc84d2f
kolomyaka1 <ka4ok24@mail.ru>
2022-04-18 23:28:18 +0300

    Создание компонента твит

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ import UserIcon from '@mui/icons-material/PersonOutline';
 const IconsList = styled("ul")`
     list-style: none;
     margin: 0;
-    padding: 0;
+    padding-top: 20px;
 `
 
 const IconsListItem = styled('li')`
@@ -27,90 +27,113 @@ const IconsListItem = styled('li')`
         font-size: 25px;
     }
 `
-const SearchTextField = styled(TextField)(({theme}) => ({
+const SearchTextField = styled(TextField)(({ theme }) => ({
 
-    div : {
+    div: {
         backgroundColor: '#E6ECF0',
         borderRadius: 30,
         border: 'none',
+        marginTop: '5px',
     },
 
-    input : {
+    input: {
         height: 25,
         padding: 0,
-    }
+        border: 'none',
+    },
+
 }))
 
+const userLogin = styled.div`
+`
+
+
 export const Home = () => {
-  return (
-    <Container maxWidth='lg'>
-        <Grid container spacing={2}>
-            <Grid item xs={3}>
-                <IconsList>
-                    <IconsListItem>
-                        <IconButton aria-label='delete' color="primary">
-                            <TwitterIcon color="primary"/>
-                        </IconButton>
-                    </IconsListItem>
-                    <IconsListItem>
-                        <IconButton aria-label='delete'>
-                            <SearchIcon />
-                        </IconButton>
-                        <Typography variant='h6'>Поиск</Typography>
-
-                    </IconsListItem>
-                    <IconsListItem>
-                        <IconButton aria-label='delete'>
-                            <NotificationIcon />
-                        </IconButton>
-                        <Typography variant='h6'>Уведомления</Typography>
-                    </IconsListItem>
-                    <IconsListItem>
-                        <IconButton aria-label='delete'>
-                            <MessageIcon />
-                        </IconButton>
-                        <Typography variant='h6'>Сообщения</Typography>
-                    </IconsListItem>
-                    <IconsListItem>
-                        <IconButton aria-label='delete'>
-                            <BookmarkBorderIcon />
-                        </IconButton>
-                        <Typography variant='h6'>Закладки</Typography>
-
-                    </IconsListItem>
-                    <IconsListItem>
-                        <IconButton aria-label='delete'>
-                            <ListIcon />
-                        </IconButton>
-                        <Typography variant='h6'>Список</Typography>
-
-                    </IconsListItem>
-                    <IconsListItem>
-                        <IconButton aria-label='delete'>
-                            <UserIcon />
-                        </IconButton>
-                        <Typography variant='h6'>Профиль</Typography>
-
-                    </IconsListItem>
-                </IconsList>
-            </Grid>
-            <Grid item xs={6}>
-                <Paper>xs</Paper>
-            </Grid>
-            <Grid item xs={3}>
-                <SearchTextField
-                    fullWidth
-                    placeholder='Поиск в Твиттере'
-                    InputProps={{ 
-                        startAdornment: (
-                            <InputAdornment position='start'>
+    return (
+        <Container maxWidth='lg'>
+            <Grid container spacing={2}>
+                <Grid item xs={3} >
+                    <IconsList>
+                        <IconsListItem>
+                            <IconButton aria-label='delete' color="primary">
+                                <TwitterIcon color="primary" />
+                            </IconButton>
+                        </IconsListItem>
+                        <IconsListItem>
+                            <IconButton aria-label='delete'>
                                 <SearchIcon />
-                            </InputAdornment>
-                        )
-                    }}
-                />
+                            </IconButton>
+                            <Typography variant='h6'>Поиск</Typography>
+
+                        </IconsListItem>
+                        <IconsListItem>
+                            <IconButton aria-label='delete'>
+                                <NotificationIcon />
+                            </IconButton>
+                            <Typography variant='h6'>Уведомления</Typography>
+                        </IconsListItem>
+                        <IconsListItem>
+                            <IconButton aria-label='delete'>
+                                <MessageIcon />
+                            </IconButton>
+                            <Typography variant='h6'>Сообщения</Typography>
+                        </IconsListItem>
+                        <IconsListItem>
+                            <IconButton aria-label='delete'>
+                                <BookmarkBorderIcon />
+                            </IconButton>
+                            <Typography variant='h6'>Закладки</Typography>
+
+                        </IconsListItem>
+                        <IconsListItem>
+                            <IconButton aria-label='delete'>
+                                <ListIcon />
+                            </IconButton>
+                            <Typography variant='h6'>Список</Typography>
+
+                        </IconsListItem>
+                        <IconsListItem>
+                            <IconButton aria-label='delete'>
+                                <UserIcon />
+                            </IconButton>
+                            <Typography variant='h6'>Профиль</Typography>
+
+                        </IconsListItem>
+                    </IconsList>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper sx={{ borderBottom: 0, borderTop: 0, height: '100vh' }} variant='outlined'>
+                        <Paper variant='outlined' sx={{ borderLeft: 0, borderRight: 0, padding: '10px 15px' }}>
+                            <Typography variant='h6' sx={{ fontWeight: 700, borderRadius: 0 }}>Главная</Typography>
+                        </Paper>
+                        <Paper variant='outlined'>
+                            <Grid container spacing={4}>
+                                <Grid item xs={1}>
+                                    <img src='https://i.pravatar.cc/45' style={{ borderRadius: '50%', margin: '7px 10px' }} alt="" />
+                                </Grid>
+                                <Grid item xs={11}>
+                                    <Typography sx={{ margin: '7px 15px' }}><b>Kolomyaka</b><span>@Kolomyaka</span></Typography>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Paper>
+
+
+                </Grid>
+                <Grid item xs={3}>
+                    <SearchTextField
+                        fullWidth
+                        placeholder='Поиск в Твиттере'
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position='start'>
+                                    <SearchIcon />
+                                </InputAdornment>
+                            )
+                        }}
+                    />
+                </Grid>
             </Grid>
-        </Grid>
-    </Container>
-  )
+        </Container>
+    )
 }
