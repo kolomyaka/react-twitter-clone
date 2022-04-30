cf398cb84064d0d4447018ebbec443aaacd1f6e4
kolomyaka1 <ka4ok24@mail.ru>
2022-05-01 00:56:11 +0300

    Попытка исправить ошибку с импортом

--- backend/server.ts
+++ backend/server.ts
@@ -6,8 +6,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/users', UserCtrl.index);
-app.post('/users', registerValidations, UserCtrl.create);
+app.get('/users');
+app.post('/users');
 
 app.listen(8888, (): void => {
     console.log('SERVER RUNNING!');
